a7d4affc72bb132e81f9885ba9c9e49864df8439
ctevnan <carolyntevnan@yahoo.com>
2016-08-24 11:33:39 -0400

    adding npm package dotenv

--- server.js
+++ server.js
@@ -1,43 +1,50 @@
+require('dotenv').config();
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+var passport = require('passport');
 var logger = require('morgan');
 var methodOverride = require('method-override');
+var server = require('http').Server(app);
 
 //config
 var db = require('./config/connection.js');
 
 //set port
-var port = process.env.PORT || 8080;
+var PORT = process.env.PORT || 8080;
 
 //connect to mongoDB
 mongoose.connect(db.url);
 
+//MIDDLEWARE
 //get all data/ of the body(POST) params
 //parse app/json
 app.use(bodyParser.json());
 
-//parse app/vnd.api and json as json
-app.use(bodyParser.json({ type: 'application/vnd.api+json'}));
+/*//parse app/vnd.api and json as json
+app.use(bodyParser.json({ type: 'application/vnd.api+json'}));*/
 
 //parse app/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-//override with the x-http-method-override header in request. simulate DEL/PUT
-app.use(methodOverride('x-http-method-override'));
+/*//override with the x-http-method-override header in request. simulate DEL/PUT
+app.use(methodOverride('x-http-method-override'));*/
 
 //set static files location /public/img will be /img for users
 app.use(express.static(__dirname + '/public'));
 
+app.use(logger('dev'));
+
 //routes
-var routes = require('./controllers/router.js');
-app.use('/', routes);
-
-//start app at localhost:8080
-app.listen(port);
-
-console.log('Listening on port ' + port);
+/*var routes = require('./controllers/router.js');
+app.use('/', routes);*/
 
 //expose app
-exports = module.exports = app;
+/*exports = module.exports = app;*/
+
+//start app at localhost:8080
+server.listen(PORT, function(){
+  console.log('Listening on ', PORT);
+}); 
