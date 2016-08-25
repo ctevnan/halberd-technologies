b223ca6ec3cd077967319e8e8b095e57da434657
ctevnan <carolyntevnan@yahoo.com>
2016-08-25 13:32:36 -0400

    adding mongoose connect to server.js

--- server.js
+++ server.js
@@ -12,25 +12,35 @@ var server = require('http').Server(app);
 //config
 var db = require('./config/connection.js');
 
+//mongoose connect
+var db = 'mongodb://localhost/halberdtechnologies';
+mongoose.connect(db, function(err){
+  if(err){
+    console.log(err);
+  } else {
+    console.log('mongoose connection is successful');
+  }
+});
+
 //set port
 var PORT = process.env.PORT || 8080;
 
 //connect to mongoDB
-mongoose.connect(db.url);
+/*mongoose.connect(db.url);*/
 
 //MIDDLEWARE
 //get all data/ of the body(POST) params
 //parse app/json
 app.use(bodyParser.json());
 
-/*//parse app/vnd.api and json as json
-app.use(bodyParser.json({ type: 'application/vnd.api+json'}));*/
+//parse app/vnd.api and json as json
+app.use(bodyParser.json({ type: 'application/vnd.api+json'}));
 
 //parse app/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
-/*//override with the x-http-method-override header in request. simulate DEL/PUT
-app.use(methodOverride('x-http-method-override'));*/
+//override with the x-http-method-override header in request. simulate DEL/PUT
+app.use(methodOverride('x-http-method-override'));
 
 //set static files location /public/img will be /img for users
 app.use(express.static(__dirname + '/public'));
@@ -39,10 +49,10 @@ app.use(logger('dev'));
 
 //routes
 /*var routes = require('./controllers/router.js');
-app.use('/', routes);*/
+app.use('/', routes);
 
 //expose app
-/*exports = module.exports = app;*/
+exports = module.exports = app;*/
 
 //start app at localhost:8080
 server.listen(PORT, function(){
