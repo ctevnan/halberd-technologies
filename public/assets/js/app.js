8e9a404dbf323340c3f4ef0a60447b543d2db2eb
ctevnan <carolyntevnan@yahoo.com>
2016-08-26 10:52:14 -0400

    setting up states in app.js

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,1 +1,24 @@
-alert('hello');
+'use strict';
+
+angular.module('App', ['ui.bootstrap', 'ui.router', 'ui-navbar'])
+
+  .config(function($stateProvider, $urlRouterProvider)) {
+
+    //for any unmatched url, redirect to /state1
+    $urlRouterProvider.otherwise("/home");
+
+    //set up states
+    $stateProvider
+      .state('home', {
+        url: "/home",
+        templateUrl: "partials/home.html"
+      })
+      .state('state1', {
+        url: "/state1",
+        templateUrl: "partials/state1.html"
+      })
+      .state('state2', {
+        url: "/state2",
+        templateUrl: "partials/state2.html"
+      });
+  })
