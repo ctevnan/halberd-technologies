246109fc620b075a9ea4edfc92dcdb3ad324c0d6
ctevnan <carolyntevnan@yahoo.com>
2016-08-26 11:08:16 -0400

    setting up navigation controller for nav

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -22,3 +22,42 @@ angular.module('App', ['ui.bootstrap', 'ui.router', 'ui-navbar'])
         templateUrl: "partials/state2.html"
       });
   })
+
+  .controller('NavigationController', function ($scope) {
+
+    $scope.tree = [{
+      name: "States",
+      link: "#",
+      subtree: [{
+          name: "state 1",
+          link: "state1"
+      },{
+          name: "state 2",
+          link: "state2",
+          subtree: [{
+              name: "state unknown",
+              link: "state unknown 2",
+          }]    
+      }]
+    },{
+      name: "No states",
+      link: "#",
+      subtree: [{
+          name: "no state connected",
+          link: "#"
+      }]
+    },{
+      name: "divider",
+      link: "#"
+
+    },{
+      name: "State has not been set up",
+      link: "#"
+    },{
+      name: "divider",
+      link: "#"
+    },{
+      name: "Again no state set up",
+      link: "#"
+    }];
+  });        
